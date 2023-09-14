72061fa07d0ff33c55528aefd9486c1d6dc5c76e
vitorrubio <vitorrubio@gmail.com>
2023-09-14 12:32:53 -0300

    sql server no bun

---
+++ bun_mssql/index.js
@@ -0,0 +1,71 @@
+    var Connection = require('tedious').Connection;  
+    var config = {  
+        server: '192.168.0.67',  //update me
+        port:1433,
+        authentication: {
+            type: 'default',
+            options: {
+                userName: 'sa', //update me
+                password: 'cAs2ptUjbEyCP%m^GKe9'  //update me
+            }
+        },
+        options: {
+            // If you are on Microsoft Azure, you need encryption:
+            encrypt: false,
+            //trustServerCertificate: true,
+            database: 'burnoutados'  //update me
+        }
+    };  
+    var connection = new Connection(config);  
+    connection.on('connect', function(err) {  
+        // If no error, then good to proceed.
+        console.log("connecting");  
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            console.log("Connected"); 
+            executeStatement();
+        }
+            
+         
+    });
+    
+    connection.connect();
+
+
+
+    var Request = require('tedious').Request;  
+    var TYPES = require('tedious').TYPES;  
+  
+    function executeStatement() {  
+        var request = new Request("select * from Users;", function(err) {  
+        if (err) {  
+            console.log(err);}  
+        });  
+        var result = "";
+          
+        request.on('row', function(columns) {  
+            columns.forEach(function(column) {  
+              if (column.value === null) {  
+                result+= "NULL" + "\t|";   
+              } else {  
+                result+= column.value + "\t|";  
+              }  
+            });  
+            console.log(result);  
+            result ="";  
+        });  
+  
+        request.on('done', function(rowCount, more) {  
+        console.log(rowCount + ' rows returned');  
+        });  
+        
+        // Close the connection after the final event emitted by the request, after the callback passes
+        request.on("requestCompleted", function (rowCount, more) {
+            connection.close();
+        });
+        connection.execSql(request);  
+    } 
