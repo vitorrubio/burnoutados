13dd3403a95fafcdbc237cb7cf504b091791434c
Vitor Luiz Rubio <vitorrubio@gmail.com>
2023-10-25 22:00:56 -0300

    refactoring 1 - multiplo de 15 e manuenibilidade

--- AgendaBeleza/AgendaBeleza/FizzBuzz/Program.cs
+++ AgendaBeleza/AgendaBeleza/FizzBuzz/Program.cs
@@ -6,17 +6,17 @@
         {
             for (int i = 0; i<=100; i++)
             {
-                if((i % 3 == 0)&&(i % 5 == 0))
+                if(i % 15 == 0)
                 {
                     Console.WriteLine("Fizz Buzz");
-                }
-                else if (i % 3 == 0)
-                {
-                    Console.WriteLine("Fizz");
                 }
                 else if (i % 5 == 0)
                 {
                     Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
                 }
                 else
                 {
