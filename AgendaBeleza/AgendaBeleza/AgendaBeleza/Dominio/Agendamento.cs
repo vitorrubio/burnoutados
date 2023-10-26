64011439c3fbbea04160a2b22be6db0b205f1df0
Vitor Luiz Rubio <vitorrubio@gmail.com>
2023-10-25 21:58:39 -0300

    fizz buzz

---
+++ AgendaBeleza/AgendaBeleza/AgendaBeleza/Dominio/Agendamento.cs
@@ -0,0 +1,9 @@
+﻿namespace AgendaBeleza.Dominio
+{
+    public class Agendamento
+    {
+        public int Id { get; set; }
+        public DateTime Inicio { get; set; }
+        public DateTime Fim { get; set; }
+    }
+}
